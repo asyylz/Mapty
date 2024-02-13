b89c9ff430db1b72f1c89e5d2aca547ba5339da7
asyylz <49663592+asyylz@users.noreply.github.com>
2024-02-13 22:24:08 +0000

    Leaflet lib  applied

--- script.js
+++ script.js
@@ -13,14 +13,27 @@ const inputElevation = document.querySelector(".form__input--elevation");
 
 /* ----------------- the Geolocation API ---------------- */
 if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-    },
-    function () {
-      alert("Geolocation may not supported by this browser or Could not get your  position.");
-    }
-  );
-}
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        const { latitude } = position.coords;
+        const { longitude } = position.coords;
+        console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+  
+        const coords = [latitude, longitude];
+        const map = L.map('map').setView(coords, 13); // 13 zoom level
+  
+        L.tileLayer('https://tile.openstreetmap.fr/hot//{z}/{x}/{y}.png', { 
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }).addTo(map);
+  
+        L.marker([coords])
+          .addTo(map)
+          .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+          .openPopup();
+      },
+      function () {
+        alert('Could not get your  position');
+      }
+    );
+  }
